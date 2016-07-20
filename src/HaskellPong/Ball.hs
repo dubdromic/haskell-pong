1129590e9cacb6f9dbaedf93b2892bc5b1a4ef54
Corin Schedler <corin@dubdromic.com>
2016-07-20 14:02:21 -0500

    Attempt at actual paddle collision physics
    This adds a commented out function to calculate a new angle for the ball
    based on where it collides with the paddle. The general idea is that the
    ball's relative position to the paddle determines the outgoing angle -
    straight out when it hits the middle, increasing up to 60 degrees
    towards the edges.
    
    The math here is (mostly) correct, but because we're on a fixed timestep
    the physics system gets confused; ocassionally, the ball ends up
    "inside" the paddle, and gets ejected at a strange (and often
    game-ending) angle. So it needs to be interpolated, or otherwise pushed
    away from the paddle reliably.

--- src/HaskellPong/Ball.hs
+++ src/HaskellPong/Ball.hs
@@ -3,10 +3,12 @@ module HaskellPong.Ball where
 import HaskellPong.Keyboard
 import HaskellPong.Tick
 import HaskellPong.Sprite
+import HaskellPong.Player
 import HaskellPong.Geometry
 import HaskellPong.Collision
 import HaskellPong.Render (Renderable(..))
-
+import Data.List
+import Data.Maybe
 
 data Ball = Ball {
   ballSprite :: Sprite
@@ -22,20 +24,33 @@ instance Tickable Ball where
 instance Collider Ball where
   vertices (Ball s) = translateQuad ballQuad $ spritePosition s
 
-collideBall :: Collider a => [a] -> Ball -> Ball
+collideBall :: [Player] -> Ball -> Ball
 collideBall a b@(Ball s) = Ball $ initSprite pos' 0 vel'
-  where paddleCollision = any (collides b) a
-        wallCollision = py >= 600 || py <= 0
+  where wallCollision = py >= 600 || py <= 0
+        collidedPaddle = find (collides b) a
         (px, py) = spritePosition s
         (vx, vy) = spriteVelocity s
         pos'
-          | paddleCollision && px >= 400 = (770, py)
-          | paddleCollision && px <= 400 = (20, py)
+          | isJust collidedPaddle && px > 400 = (770, py)
+          | isJust collidedPaddle && px < 400 = (20, py)
           | otherwise = (px, py)
         vel'
-          | paddleCollision = (-vx, vy)
+          | isJust collidedPaddle = (-vx * 1.01, vy * 1.01) -- ballCollidedVelocity b $ fromJust collidedPaddle
           | wallCollision   = (vx, -vy)
           | otherwise       = spriteVelocity s
+
+-- ballCollidedVelocity :: Ball -> Player -> PVector2
+-- ballCollidedVelocity b@(Ball bs) p@(Player ps) = (vx', vy')
+--   where vx = if paddleX > 400 then -vx' else vx'
+--         vy = -vy'
+--         vx' = 5 * cos angle
+--         vy' = 5 * sin angle
+--         angle = ni * 60
+--         ni = riy / 35
+--         riy = (paddleY + 35) - (ballY + 5)
+--         (paddleX, paddleY) = spritePosition ps
+--         (_, ballY) = spritePosition bs
+
 
 tickBall :: Keyboard -> Ball -> Ball
 tickBall kb (Ball s) = Ball $ initSprite pos 0 vel
